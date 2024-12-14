79cec05b639c0f61e466c34d8d3b4e0fa3bf4ca3
ShubhamVerma-Developer <shubham25122001@gmail.com>
2024-12-14 20:13:16 +0530

    "feat: add input and label components for login page design"

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -1,3 +1,7 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import Link from "next/link";
 import React from "react";
 
 export default function login() {
@@ -9,6 +13,32 @@ export default function login() {
         </h1>
         <h1 className="text-3xl font-bold">Login</h1>
         <p>Welcome back</p>
+        <form>
+          <div className="mt-4">
+            <Label htmlFor="email">Email</Label>
+            <Input id="email" name="email" placeholder="Enter your Email.." />
+          </div>
+          <div className="mt-4">
+            <Label htmlFor="password">Password</Label>
+            <Input
+              id="password"
+              name="password"
+              placeholder="Enter your password.."
+            />
+            <div className="text-right font-bold">
+              <Link href="forget-password">Forget Password?</Link>
+            </div>
+          </div>
+          <div className="mt-4">
+            <Button className="w-full">Submit</Button>
+          </div>
+        </form>
+        <p className="text-center mt-2">
+          Don't have an account ?{" "}
+          <strong>
+            <Link href="/register">Register</Link>
+          </strong>
+        </p>
       </div>
     </div>
   );
