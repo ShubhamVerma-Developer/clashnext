6643585870969c305279e866c9101e020cf78d70
ShubhamVerma-Developer <shubham25122001@gmail.com>
2024-12-15 15:39:40 +0530

    chore: Setup EJS templating and file watching for development - Installed EJS and configured it as the view engine. - Added file copy setup for static assets. - Installed necessary development dependencies (e.g., nodemon) for live reload. - Configured nodemon to watch changes in views for seamless development. - Tested the setup to ensure proper rendering and reload functionality.

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,21 @@
 import express, { Application, Request, Response } from "express";
 import "dotenv/config";
+import path from "path";
+import { fileURLToPath } from "url";
 
 const app: Application = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 7000;
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+// * Set View engine
+app.set("view engine", "ejs");
+app.set("views", path.resolve(__dirname, "./views"));
 
 app.get("/", (req: Request, res: Response) => {
-    res.send("Hey It's working........ 👌");
+    res.render("welcome");
 });
 
 app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`));
