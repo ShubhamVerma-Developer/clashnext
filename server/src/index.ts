1930c6a15d8b6be4a63d64f67816bd80aa286790
ShubhamVerma-Developer <shubham25122001@gmail.com>
2024-12-15 17:04:08 +0530

    feat: Configure email service with Nodemailer and send emails dynamically
    - Added Nodemailer configuration in confiig/mail.js to enable SMTP email sending.
    - Set up transporter using environment variables for SMTP host, user, and password.
    - Implemented sendEmail utility function to send emails with dynamic recipient, subject, and HTML body.
    - Integrated EJS as the templating engine to render dynamic email templates.
    - Created a sample route (/) to send a test email using a welcome template (welcome.ejs).
    - Configured the view engine and paths for EJS templates in the application.
    - Added dotenv support for managing environment variables securely.
    
    Tested the email service with a sample SMTP server.

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,6 +2,8 @@ import express, { Application, Request, Response } from "express";
 import "dotenv/config";
 import path from "path";
 import { fileURLToPath } from "url";
+import ejs from "ejs";
+import { sendEmail } from "./confiig/mail.js";
 
 const app: Application = express();
 const PORT = process.env.PORT || 7000;
@@ -14,8 +16,10 @@ app.use(express.urlencoded({extended: false}));
 app.set("view engine", "ejs");
 app.set("views", path.resolve(__dirname, "./views"));
 
-app.get("/", (req: Request, res: Response) => {
-    res.render("welcome");
+app.get("/", async(req: Request, res: Response) => {
+    const html = await ejs.renderFile(__dirname + `/views/emails/welcome.ejs`, {name: "shubham verma"});
+    await sendEmail("vagaya1617@rustetic.com", "Testing SMTP", html)
+    res.json({msg: "Email sent successfully!"});
 });
 
 app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`));
