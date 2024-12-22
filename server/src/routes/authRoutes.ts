c8363356d43b48811d8ccd2ff96741dad3244e08
ShubhamVerma-Developer <shubham25122001@gmail.com>
2024-12-22 10:00:48 +0530

    fix(express): resolve "No overload matches this call" issue by downgrading express version
    - Downgraded the express version to ensure compatibility with TypeScript typings.
    - Fixed type mismatch errors causing the "No overload matches this call" issue.
    - Verified application functionality with the downgraded version.

--- server/src/routes/authRoutes.ts
+++ server/src/routes/authRoutes.ts
@@ -1,7 +1,9 @@
-import { Router, Request, Response } from "express";
+import { Router, Response, Request } from "express";
 import { registerSchema } from "../validation/authValidations.js";
 import { ZodError } from "zod";
 import { formateError } from "../helper.js";
+import prisma from "../confiig/database.js";
+import bcrypt from "bcrypt";
 
 const router = Router();
 
@@ -10,16 +12,33 @@ router.post("/register", async (req: Request, res: Response) => {
     try {
         const body = req.body;
         const payload = registerSchema.parse(body);
-        res.json(payload); 
+        let user = await prisma.user.findUnique({where: {email: payload.email}});
+        if (user) {
+            return res.status(422).json({errors: { email: "Email already taken. please use another one.",}});
+        }
+
+        // * Encrypt the password 
+        const salt = await bcrypt.genSalt(10);
+        console.log(salt);
+        
+        payload.password = await bcrypt.hash(payload.password, salt);
+
+        await prisma.user.create({
+            data:{
+            name: payload.name,
+            email: payload.email,
+            password: payload.password,
+            }
+        });
+
+        return res.status(201).json({message: "Account created successfylly."});
     } catch (error) {
         if (error instanceof ZodError) {
             const errors = formateError(error);
-            res.status(422).json({message: "Invalid data", errors}); 
+            return res.status(422).json({message: "Invalid data", errors}); 
         }
-        res.status(500).json({message: "Something went wrong!. Please try again."});
+        return res.status(500).json({message: "Something went wrong!. Please try again."});
     }
 });
-
-
 
 export default router;
